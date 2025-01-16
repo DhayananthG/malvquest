976e4ee5913c148a75ff2691b3b011b945494122
DhayananthG <iamdhayaoffl@gmail.com>
2025-01-16 22:03:28 +0530

    Packages Intalled

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Update paths to your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
