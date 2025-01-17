fd0de404ecf8ebe578a789bbf62d5de04acf71c4
DhayananthG <iamdhayaoffl@gmail.com>
2025-01-17 20:12:49 +0530

    Home Page,Navbar,Mobile menu Fixed

--- script.js
+++ script.js
@@ -11,3 +11,49 @@ let texts = [
     "From Here to Anywhere – Study Abroad!"
   ];
   
+  let textElement = document.getElementById('animated-text');
+  
+  let num = 0;
+  const n = texts.length;
+  
+  const change_text = () => {
+    textElement.innerText = texts[num];
+  
+    // Animate text
+    const letters = textElement.innerText.split('');
+    textElement.innerHTML = letters.map((letter, index) => {
+      const char = letter === ' ' ? '&nbsp;' : letter;
+      return `<span style="animation-delay: ${index * 0.1}s; animation: fadeIn 0.1s;">${char}</span>`;
+    }).join('');
+  
+    num = (num + 1) % n;
+    setTimeout(change_text, 5000);
+  };
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    change_text();
+  });
+  
+  const sidebar = document.getElementById('sidebar');
+  const sidebarOpen = document.getElementById('sidebarOpen');
+  const sidebarClose = document.getElementById('sidebarClose');
+  
+  sidebarOpen.addEventListener('click', () => {
+    sidebar.classList.remove('-translate-x-full');
+  });
+  
+  sidebarClose.addEventListener('click', () => {
+    sidebar.classList.add('-translate-x-full');
+  });
+  
+  const navbar = document.getElementById('navbar');
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 20) {
+      navbar.classList.add('bg-white', 'shadow-md', 'text-black');
+      navbar.classList.remove('bg-transparent', 'text-white');
+    } else {
+      navbar.classList.add('bg-transparent', 'text-white');
+      navbar.classList.remove('bg-white', 'shadow-md', 'text-black');
+    }
+  });
+  
