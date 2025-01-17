9254f4a8e49761f4a476cc521a8a06f2c9f7c2c7
DhayananthG <iamdhayaoffl@gmail.com>
2025-01-17 08:35:50 +0530

    About,Directors,Conatct,Footer Fixed

---
+++ script.js
@@ -0,0 +1,13 @@
+let texts = [
+    "Go Global, Learn Limitless!",
+    "Study Abroad, Shape Tomorrow!",
+    "Dream Bigger, Study Further!",
+    "Your Global Journey Begins Today!",
+    "Higher Studies, Higher Horizons!",
+    "Discover, Learn, Conquer – Abroad!",
+    "Step Out, Stand Out – Study Abroad!",
+    "Expand Minds, Explore Borders!",
+    "Think Global, Learn Universal!",
+    "From Here to Anywhere – Study Abroad!"
+  ];
+  
